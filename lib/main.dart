51a5f2e6c60c330643acbe693b5927d3272b4e3d
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-04-30 11:22:59 +0530

    Align the image to the center of the body

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,11 @@ void main() {
           title: Text('I am Rich'),
           backgroundColor: Colors.blueGrey[900],
         ),
-        body: Image(
-          image: NetworkImage(
-              'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'),
+          ),
         ),
       ),
     ),
