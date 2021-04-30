30dbf622f2ef1bbda3b324cc7eeeb5ba4f15ddd2
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-04-30 14:58:48 +0530

    Remove the NetworkImage() and add AssetImage() and pass the name of the asset to it as mentioned in pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,7 @@ void main() {
         ),
         body: Center(
           child: Image(
-            image: NetworkImage(
-                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
