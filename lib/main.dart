c49bf49cb6552b9c840de27d2078b2c89c1d6801
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-04-30 11:08:57 +0530

    Add background color for the entire Scaffold() class

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey[900],
         appBar: AppBar(
           title: Text('I am Rich'),
           backgroundColor: Colors.blueGrey[900],
