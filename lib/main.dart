3c39b2fbb01e7681735facdcd2dc3c40c9de6f66
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-04-30 10:42:16 +0530

    Add comma's after every closing parenthesis to reformat the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Center(child: Text('Hello World'))));
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  );
 }
