95df4847c5e00756bf1d109078b6b9f94280b1f9
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-04-30 10:38:55 +0530

    Add center widget for aligning the text in the center

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Text('Hello World'),
-  ));
+  runApp(MaterialApp(home: Center(child: Text('Hello World'))));
 }
